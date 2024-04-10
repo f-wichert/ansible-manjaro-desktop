a9d4ca52e47fc2cfd4fdfebcbf37b3c2076839a3
f-wichert <50278804+f-wichert@users.noreply.github.com>
2024-04-10 21:18:52 +0200

    The start.sh script should now work, though it is not fully functional (like tags etc.)

--- start.sh
+++ start.sh
@@ -8,26 +8,19 @@ fi
 
 
 
-# User input
-echo -e "\n"
-echo "Enter your username:"
-read -e -p "Enter Ubuntu username: ($SUDO_USER) " -i $SUDO_USER user_login
-
-
-
 # System update
-#read -e -p "Do you want to start a full system upgrade? [Y/n]" -i 'Y' response
-read -e -p "Do you want to start a full system upgrade? [Y/n] " response
+read -e -p "Do you want to start a full system upgrade? [Y/n] " -i 'n' response
 
 while true; do
   case "$response" in
       [yY][eE][sS]|[yY])
         echo "Starting full system upgrade..."
+        sudo pacman -Syu
         break
         ;;
       *)
         echo "Continuing without system upgrade..."
-        exit
+        break
         ;;
   esac
 done
@@ -35,21 +28,30 @@ done
 
 # User Input
 echo -e "\n"
-echo "Enter the following information: (TODO)"
+echo "Enter the following information:"
 
-read "Do you want to start a full system upgrade? [Y,/n]" -i 'Y' response
+echo -e "\n"
+echo "Enter your username:"
+read -e -p "Enter Ubuntu username: ($SUDO_USER) " -i $SUDO_USER user_login
 
-# Begin of installation
 
-# Full system upgrade
-echo "Full system upgrade..."
-sudo pacman -Syu
 
 # Install ansible
 echo "Installing ansible..."
-# sudo pacman -S ansible
 yay -S ansible-core
 ansible-galaxy collection install community.general
 
 # Execute ansible
-ansible-pull -K -U https://github.com/f-wichert/ansible-manjaro-desktop
+ansible-pull -K -U https://github.com/f-wichert/ansible-manjaro-desktop \
+  -e "user_login=$user_login" \
+  #-e "cron_job_name='first install'" \
+  #-e "git_user_name=$git_user_name" \
+  #-e "git_user_email=$git_user_email" \
+
+
+# use --tags "tag1,tag2" to run only the tasks tagged with tag1 and tag2
+
+# use --tags "always" to run only the tasks tagged with "always" (basically the basic stuff)
+# run without --tags to run everything
+
+# use --check --diff to make a dry run and see what would change
