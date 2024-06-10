714dbeb0a177052a05a53737900a29891d82fa0c
f-wichert <50278804+f-wichert@users.noreply.github.com>
2024-06-10 21:16:11 +0200

    Added mouse config and some more user input.

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 source ./scripts/multiple-choice.sh
 
 
-# System update
+## System update ##
 read -e -p "Do you want to start a full system upgrade? [Y/n] " -i 'Y' response
 
 while true; do
@@ -20,39 +20,7 @@ while true; do
 done
 
 
-# User Input
-echo -e "\n"
-echo "Enter the following information::"
-#echo "Enter the following information [arrows:move - space:select - enter:continue]:"
-
-echo -e "\n"
-echo "Enter your username:"
-read -e -p "Enter manjaro username: " user_login
-
-
-
-# Usage Example
-# OPTIONS_VALUES=("packages" "oh-my-zsh" "i3" "polybar")
-# OPTIONS_LABELS=("packages" "oh-my-zsh" "i3" "polybar")
-
-# for i in "${!OPTIONS_VALUES[@]}"; do
-# 	#OPTIONS_STRING+="${OPTIONS_VALUES[$i]} (${OPTIONS_LABELS[$i]});"
-# 	OPTIONS_STRING+="${OPTIONS_VALUES[$i]};"
-# done
-
-# prompt_for_multiselect SELECTED "$OPTIONS_STRING"
-
-# for i in "${!SELECTED[@]}"; do
-# 	if [ "${SELECTED[$i]}" == "true" ]; then
-# 		CHECKED+=("${OPTIONS_VALUES[$i]},")
-# 	fi
-# done
-# CHECKED=${CHECKED[@]::-1}
-# echo "${CHECKED}"
-
-
-
-# Install ansible
+## Install ansible ##
 read -e -p "Do you want to install ansible + requirements? [Y/n] " -i 'Y' response
 
 while true; do
@@ -72,11 +40,48 @@ while true; do
 done
 
 
+## User Input ##
+#echo -e "\n"
+#echo "Enter the following information:"
+
+echo -e "\n"
+echo "Which .dotfile to use:"
+read -e -p "Do you want to use your own .dotfiles repo? Otherwises uses pre-packaged dotfiles [Y/n] " -i 'N' response
+
+
+echo -e "\n"
+echo "Enter your username:"
+read -e -p "Enter manjaro username: " user_login
+read -e -p "Enter git username: " git_user_login
+read -e -p "Enter git email: " git_user_email
+
+
+## Module Selection ##
+# OPTIONS_VALUES=("packages" "oh-my-zsh" "i3" "polybar")
+# OPTIONS_LABELS=("packages" "oh-my-zsh" "i3" "polybar")
+
+# for i in "${!OPTIONS_VALUES[@]}"; do
+# 	#OPTIONS_STRING+="${OPTIONS_VALUES[$i]} (${OPTIONS_LABELS[$i]});"
+# 	OPTIONS_STRING+="${OPTIONS_VALUES[$i]};"
+# done
+
+# prompt_for_multiselect SELECTED "$OPTIONS_STRING"
+
+# for i in "${!SELECTED[@]}"; do
+# 	if [ "${SELECTED[$i]}" == "true" ]; then
+# 		CHECKED+=("${OPTIONS_VALUES[$i]},")
+# 	fi
+# done
+# CHECKED=${CHECKED[@]::-1}
+# echo "${CHECKED}"
+
+
 # Execute ansible
 ansible-pull -K -U https://github.com/f-wichert/ansible-manjaro-desktop \
   -e "user_login=$user_login" \
+  -e "dotfile_location=$dotfile_location" \
   #-e "cron_job_name='first install'" \
-  #-e "git_user_name=$git_user_name" \
+  -e "git_user_name=$git_user_name" \
   #-e "git_user_email=$git_user_email" \
   --tags all
 
